a29ae05337ba13603875bd6ecce93e99e3de5949
Santhosh Raju <santhoshraju@Santhoshs-MacBook-Pro.local>
2017-03-13 10:21:03 +0530

    Adding cross domain origin access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,9 @@ class ApplicationController < ActionController::Base
 
 
   def set_cross_domain_headers
-    response.headers['Access-Control-Allow-Origin'] = "*"
-    response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
